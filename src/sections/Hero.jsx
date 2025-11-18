0022add54943153c9508788fc6fa113a7324a598
Alejandro2097 <alejo12381@hotmail.es>
2025-11-18 17:22:47 -0500

    liting

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,16 @@ const Hero = () => {
   return (
     <section className="hero-sectio">
         <div className="size-full mask-wrapper">
-            <img src="/images/hero-bg.webp" alt="background" className="scale-out"/>
-            <img src="/images/hero-text.webp" alt="hero-logo" className="title-logo fade-out"/>
+            <img 
+                src="/images/hero-bg.webp" 
+                alt="background" 
+                className="scale-out"
+            />
+            <img 
+                src="/images/hero-text.webp" 
+                alt="hero-logo" 
+                className="title-logo fade-out"
+            />
         </div>
     </section>
   )
