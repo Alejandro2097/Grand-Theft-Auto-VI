a88ddd320dcbb68e97f720c6b39f2f7362565668
Alejandro2097 <alejo12381@hotmail.es>
2025-11-24 18:39:14 -0500

    llamado

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,33 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
-import FirstVideo from './sections/FirstVideo';
+import NavBar from './sections/NavBar';
 import Hero from './sections/Hero';
-import Navbar from './sections/Navbar';
+import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
+import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
+import Final from './sections/Final';
+import Outro from './sections/Outro';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Hero/>
+      <NavBar />
+      <Hero />
 
       <FirstVideo />
+      <Jason />
+
+      <SecondVideo />
+      <Lucia />
+
+      <PostCard />
+      <Final />
+      <Outro />
     </main>
   )
 }
